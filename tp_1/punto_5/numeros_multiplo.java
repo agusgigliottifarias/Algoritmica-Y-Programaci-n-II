3d0ef4949b8027e1cd91815032d8ace4d617fe84
Guillermo Gigliotti Farias <agustingigliottifarias@gmail.com>
2025-03-31 20:07:54 -0300

    Delete tp_1 directory

--- tp_1/punto_5/numeros_multiplo.java
+++
@@ -1,8 +0,0 @@
-package tp_1.punto_5;
-public class numeros_multiplo {
-
-    // Método que verifica si num1 es múltiplo de num2
-    public static boolean esMultiplo(int num1, int num2) {
-        return num1 % num2 == 0;
-    }
-}
--- tp_1/punto_7/test_TablaPotencias.java
+++
@@ -1,8 +0,0 @@
-package tp_1.punto_7;
-
-public class test_TablaPotencias {
-    public static void main(String[] args) {
-        // Llamamos a imprimirTabla() de TablaPotencias
-        TablaPotencias.imprimirTabla();
-    }
-}
